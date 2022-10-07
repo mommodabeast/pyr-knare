0b5743f6261b577cd75a8de21f344a6adc8fcc6f
Ria <kristoffer.berg8@gmail.com>
2022-10-07 11:26:33 +0200

    Deleted previous project files

--- error_checker.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def error_checker(expression):
-    result1 = re.search(r"[+-]?-\d+.\d+ -\d+|-\d+.\d+ \d+|-\d+ -\d+|-\d+.\d+ \d+|-\d+ \d+|\d+.\d+ -\d+|\d+ -\d+|\d+ \d+" ,expression)
-    if result1 == None:
-        print("Right!")
-    else:
-        print("Wrong!")
- #   result2 = re.search(r"[+-]?[+/-][-/+]|[+/-][+/-]")
